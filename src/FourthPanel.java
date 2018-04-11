e2ed85f4625d2f629d00d306f3c0ff6936719f0d
Alexander Davis <alexanderdavis.net@googlemail.com>
2018-04-11 21:57:53 +0100

    comments

--- src/FourthPanel.java
+++ src/FourthPanel.java
@@ -6,8 +6,6 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.collections.FXCollections;
-
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import javafx.scene.layout.HBox;
@@ -21,20 +19,23 @@ import javafx.scene.layout.HBox;
  * @version 29.03.2018
  */
 public class FourthPanel extends PropertyPanel {
-
     private GridPane panel;
     private ListView<AirbnbListing> list = new ListView<>();
-    private ObservableList<AirbnbListing> items;
     private SentimentAnalysis analyser;
+    // Side panel with info about selected property.
     private BorderPane informationPanel;
+    // Box to hold sentiment rating.
     private HBox smilyBox;
+    // Current selected property details.
     private String currentPropertyID;
     private List<String> currentReviews;
 
     public FourthPanel() {
         title = "Analysis";
         panel = new GridPane();
+        // set listView cell factory.
         list.setCellFactory(param -> new ListingCell());
+        informationPanel = new BorderPane();
         analyser = new SentimentAnalysis(this);
 
         panelSetUp();
@@ -50,7 +51,7 @@ public class FourthPanel extends PropertyPanel {
      * to the price range specified.
      */
     public void update() {
-        items = FXCollections.observableArrayList(CurrentListings.getCurrentListings());
+        ObservableList<AirbnbListing> items = FXCollections.observableArrayList(CurrentListings.getCurrentListings());
         list.setItems(items);
     }
 
@@ -77,26 +78,31 @@ public class FourthPanel extends PropertyPanel {
     }
 
     /**
-     * Set up the lay out of the panel.
+     * Set up the layout of the panel.
      */
     private void panelSetUp() {
+        // Add listview to left column.
         panel.add(list, 0, 0);
-        informationPanel = new BorderPane();
-        informationPanel.setMinWidth(200);
+        // Add informationPanel to right column.
+        panel.add(informationPanel, 1, 0);
+        // Initial instructions on right panel.
         informationPanel.setCenter(new Label("Click on one " +
                 "item of the list on the left and \n see the sentiment score and details!"));
+        // Limit information panel height to panel height.
         informationPanel.setMaxHeight(panel.heightProperty().get());
+        // Smiley box holds sentiment analysis rating.
         smilyBox = new HBox(5);
-        panel.add(informationPanel, 1, 0);
+        // Set right column and left column sizes.
         ColumnConstraints column1 = new ColumnConstraints();
         column1.setPercentWidth(60);
         ColumnConstraints column2 = new ColumnConstraints();
         column2.setPercentWidth(40);
-        panel.getColumnConstraints().addAll(column1, column2); // each get 50% of width
+        panel.getColumnConstraints().addAll(column1, column2); // Column 1, 60%. Column 2, 40%.
         RowConstraints row1 = new RowConstraints();
         row1.setPercentHeight(100);
-        panel.getRowConstraints().addAll(row1); // each get 50% of width
+        panel.getRowConstraints().addAll(row1); // Height is 100%.
 
+        // Add listener for listView item selected.
         list.getSelectionModel().selectedIndexProperty().addListener(
                 (ov, old_val, new_val) -> {
                     // When the multirange slider is moved it returns -1 as new_val, and then returns actual value.
@@ -105,12 +111,15 @@ public class FourthPanel extends PropertyPanel {
                     if(new_val.intValue() == -1 || old_val.equals(new_val)) {
                         return;
                     }
+                    // Get ID of property selected.
                     currentPropertyID = CurrentListings.getCurrentListings().get(new_val.intValue()).getId();
                     try {
+                        // Get reviews for ID.
                         currentReviews = AirbnbAPI.getReviewComments(currentPropertyID);
                         List<String> reviews = currentReviews;
+                        // Start analysis.
                         analyser = new SentimentAnalysis(this);
-                                analyser.execute(String.join(", ", reviews));
+                        analyser.execute(String.join(", ", reviews));
                     } catch (Exception e) {
                         if (e.getLocalizedMessage().contains("403")) {
                             informationPanel.getChildren().clear();
@@ -124,16 +133,26 @@ public class FourthPanel extends PropertyPanel {
                 });
     }
 
+    /**
+     * Called when a listing is clicked and sentiment analysis is calculating.
+     */
     public void updateProgressLabel() {
         informationPanel.getChildren().clear();
         informationPanel.setCenter(new Label("Calculating sentiment analysis..."));
     }
 
+    /**
+     * Updates informationPanel with new information from user selection.
+     * Called by sentiment analysis when complete.
+     * @param score
+     */
     public void updateListingPanel(int score) {
         informationPanel.getChildren().clear();
         VBox top = new VBox();
+        // Sentiment score.
         addSmilyFace(score);
         top.getChildren().add(smilyBox);
+        // Get listing image from Airbnb.
         ImageView listingImage;
         try {
             listingImage = new ImageView(AirbnbAPI.getListingImage(currentPropertyID));
@@ -146,25 +165,27 @@ public class FourthPanel extends PropertyPanel {
         }
         informationPanel.setTop(top);
 
-            ArrayList<Label> reviewLabels = new ArrayList<>();
-            for (String review : currentReviews) {
-                Label label = new Label(review);
-                label.setMaxWidth(300);
-                label.setWrapText(true);
-                reviewLabels.add(label);
-            }
-            VBox reviewLabelList = new VBox(20);
-            reviewLabelList.getChildren().addAll(reviewLabels);
-            ScrollPane reviewBox = new ScrollPane();
-            reviewBox.setMaxHeight(200);
-            reviewBox.setContent(reviewLabelList);
-            informationPanel.setBottom(reviewBox);
-
+        // Create label for each review.
+        ArrayList<Label> reviewLabels = new ArrayList<>();
+        for (String review : currentReviews) {
+            Label label = new Label(review);
+            label.setMaxWidth(300);
+            label.setWrapText(true);
+            reviewLabels.add(label);
+        }
+        // Add all reviews to VBox.
+        VBox reviewLabelList = new VBox(20);
+        reviewLabelList.getChildren().addAll(reviewLabels);
+        // Add reviews VBox to scrollPane.
+        ScrollPane reviewBox = new ScrollPane();
+        reviewBox.setMaxHeight(200);
+        reviewBox.setContent(reviewLabelList);
+        informationPanel.setBottom(reviewBox);
     }
 }
 
 /**
- * This class is responsible for the individual cell of the list that is on the right hand side
+ * This class is responsible for the individual cell of the list that is on the left hand side
  * of the panel to display the current listing.
  */
 class ListingCell extends ListCell<AirbnbListing> {
